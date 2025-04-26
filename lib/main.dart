87008a14d1f93b54d2182afded1bbdcf2f7d6518
DevinderSingh <sdevider@gmail.com>
2025-04-26 13:58:59 +0530

    Update: WebView working

--- lib/main.dart
+++ lib/main.dart
@@ -2,32 +2,107 @@ import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
-  runApp(const MaterialApp(home: WebViewApp()));
+  runApp(const MyApp());
 }
 
-class WebViewApp extends StatefulWidget {
-  const WebViewApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
-  State<WebViewApp> createState() => _WebViewAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Musi Clone')),
+        body: const YouTubeWebView(),
+      ),
+    );
+  }
 }
 
-class _WebViewAppState extends State<WebViewApp> {
+class YouTubeWebView extends StatefulWidget {
+  const YouTubeWebView({super.key});
+
+  @override
+  State<YouTubeWebView> createState() => _YouTubeWebViewState();
+}
+
+class _YouTubeWebViewState extends State<YouTubeWebView> {
   late final WebViewController _controller;
+  final TextEditingController _searchController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
     _controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(Colors.black)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onPageStarted: (String url) {
+            debugPrint('Loading: $url');
+          },
+          onPageFinished: (String url) {
+            debugPrint('Loaded: $url');
+            // Inject JavaScript to ensure audio continues in background
+            _controller.runJavaScript('''
+              var videos = document.getElementsByTagName("video");
+              for (var i = 0; i < videos.length; i++) {
+                videos[i].play();
+              }
+            ''');
+          },
+          onWebResourceError: (WebResourceError error) {
+            debugPrint('Error: ${error.description}');
+          },
+          onNavigationRequest: (NavigationRequest request) {
+            if (request.url.contains('youtube.com') ||
+                request.url.contains('googlevideo.com')) {
+              return NavigationDecision.navigate;
+            }
+            return NavigationDecision.prevent;
+          },
+        ),
+      )
       ..loadRequest(Uri.parse('https://m.youtube.com'));
+  }
+
+  void _searchYouTube(String query) {
+    if (query.isNotEmpty) {
+      final url =
+          'https://m.youtube.com/results?search_query=${Uri.encodeQueryComponent(query)}';
+      _controller.loadRequest(Uri.parse(url));
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Musi Clone 🎵')),
-      body: WebViewWidget(controller: _controller),
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextField(
+            controller: _searchController,
+            decoration: InputDecoration(
+              labelText: 'Search YouTube',
+              suffixIcon: IconButton(
+                icon: const Icon(Icons.search),
+                onPressed: () => _searchYouTube(_searchController.text),
+              ),
+              border: const OutlineInputBorder(),
+            ),
+            onSubmitted: _searchYouTube,
+          ),
+        ),
+        Expanded(
+          child: WebViewWidget(controller: _controller),
+        ),
+      ],
     );
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
   }
 }
